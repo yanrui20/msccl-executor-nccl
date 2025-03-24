b9d64aac0a7ea10483e178a500319ec4fcfa49d3
yanrui <1657066071@qq.com>
2025-03-24 22:13:18 +0800

    增加DEFINE_ncclDevFunc的行数，并打印输出

--- src/device/common.h
+++ src/device/common.h
@@ -393,13 +393,16 @@ __device__ __forceinline__ void ncclKernelMain(struct ncclDevKernelArgs const* a
 __global__ void ncclDevKernel_Generic(ncclDevKernelArgs4K NCCL_GRID_CONSTANT const args4K);
 __device__ void ncclDevFunc_Nop();
 
+#define STRINGIFY(x) #x
 #define DEFINE_ncclDevKernel(suffix, coll, redop, ty, algo, proto, specializedFnId) \
   __global__ void ncclDevKernel_##suffix(ncclDevKernelArgs4K NCCL_GRID_CONSTANT const args4K) { \
+    printf("go into DEFINE_ncclDevKernel_%s", STRINGIFY(suffix)); \
     ncclKernelMain<specializedFnId, RunWorkBatch<coll, ty, redop<ty>, algo, proto>>(&args4K.args); \
   }
 
 #define DEFINE_ncclDevFunc(suffix, coll, redop, ty, algo, proto) \
   __device__ void ncclDevFunc_##suffix() { \
+    printf("go into DEFINE_ncclDevFunc_%s", STRINGIFY(suffix)); \
     RunWorkBatch<coll, ty, redop<ty>, algo, proto>().run(); \
   }
 
