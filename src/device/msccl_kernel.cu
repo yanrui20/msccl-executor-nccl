842e81cffdb35ccfe4dce3a589c1fed1f13de2dd
yanrui <1657066071@qq.com>
2025-03-24 23:15:48 +0800

    打印print_break_iter

--- src/device/msccl_kernel.cu
+++ src/device/msccl_kernel.cu
@@ -19,8 +19,8 @@ __shared__ struct mscclShmemData mscclShmem;
 
 __shared__ struct mscclShmemData depShmem;
 
-#define print_break_iter(bid, step, break_iter) \
-  if (tid == 0 && false) printf("where to break: bid %d, step %d, break_iter %d\n", bid, step, break_iter++);
+#define print_break_iter(bid, step) \
+  if (tid == 0) printf("where to break: bid %d, step %d, line %d\n", bid, step, __LINE__);
 
 #define MSCCL_MAX_ITER 65536
 #define DEBUG_PRINT 0
@@ -233,28 +233,28 @@ __device__ __forceinline__ void mscclRunInterpreter(
     int step = 0;
     for (int i = 0; i < mscclShmem.mscclTB.nSteps; i++){
       uint8_t break_iter = 0;
-      print_break_iter(bid, i, break_iter);
+      print_break_iter(bid, i);
       struct mscclTransmission* t = &mscclShmem.mscclTB.transmissions[i];
-      print_break_iter(bid, i, break_iter);
+      print_break_iter(bid, i);
       // first wait if there is a dependence
       int16_t numDependencies = t->numDependencies;
-      print_break_iter(bid, i, break_iter);
+      print_break_iter(bid, i);
       if (numDependencies > 0){
         if (tid < numDependencies) {
-          print_break_iter(bid, i, break_iter);
+          print_break_iter(bid, i);
           int16_t dependentPointer = t->dependencePointer;
-          print_break_iter(bid, i, break_iter);
+          print_break_iter(bid, i);
           int8_t dependentBid = mscclShmem.mscclTB.dependentBid[dependentPointer+tid];
-          print_break_iter(bid, i, break_iter);
+          print_break_iter(bid, i);
           int16_t dependentStep = mscclShmem.mscclTB.dependentStep[dependentPointer+tid];
-          print_break_iter(bid, i, break_iter);
+          print_break_iter(bid, i);
           uint64_t goalFlag = COMPUTE_FLAG(workIndex, iter, dependentStep);
-          print_break_iter(bid, i, break_iter);
+          print_break_iter(bid, i);
           while (true){
             uint64_t curFlag = (mscclFlags + dependentBid)->flag;
-            print_break_iter(bid, i, break_iter);
+            print_break_iter(bid, i);
             if (curFlag >= goalFlag && GET_WORKINDEX_FROM_FLAG(curFlag) == workIndex) break;
-            print_break_iter(bid, i, break_iter);
+            print_break_iter(bid, i);
           }
         }
         step += numDependencies-1;
