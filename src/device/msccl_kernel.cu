4442b9dbdceaf890cef59b08ffae61db8fa64bac
yanrui <1657066071@qq.com>
2025-03-24 16:09:59 +0800

    cuda print

--- src/device/msccl_kernel.cu
+++ src/device/msccl_kernel.cu
@@ -10,6 +10,7 @@
 
 #include "msccl/msccl_struct.h"
 #include "msccl/msccl_kernel.h"
+#include <stdio.h>
 
 #if defined(ENABLE_NPKIT)
 #include "npkit/npkit.h"
@@ -87,6 +88,7 @@ __device__ __forceinline__ void mscclRunInterpreter(
   const int tid = threadIdx.x;
   const int bid = blockIdx.x;
   const int nthreads = blockDim.x;
+  printf("cuda debug helloworld");
 
   // initialize mscclShmem.mscclTB
   threadBlockCopy(
