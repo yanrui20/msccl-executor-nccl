5f8ef1dc9ae9d48d7d61f6a4406fbd0000c613c7
yanrui <1657066071@qq.com>
2025-03-25 17:51:53 +0800

    将MAXCHANNELS 从32改成64，以支持64ch的实验

--- src/include/device.h
+++ src/include/device.h
@@ -62,7 +62,7 @@ union ncclLLFifoLine {
 };
 
 #define WARP_SIZE 32
-#define MAXCHANNELS 32
+#define MAXCHANNELS 64
 #define NCCL_MAX_LOCAL_RANKS 64
 #define NCCL_MAX_NTHREADS 640
 #define NCCL_MIN_NTHREADS (4*WARP_SIZE)
