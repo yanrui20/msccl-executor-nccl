0b161e56ea9dad5191d18bdb014dd6b79d7a3ff5
yanrui <1657066071@qq.com>
2025-03-25 16:17:27 +0800

    打开打印

--- src/device/msccl_kernel.cu
+++ src/device/msccl_kernel.cu
@@ -20,7 +20,7 @@ __shared__ struct mscclShmemData mscclShmem;
 __shared__ struct mscclShmemData depShmem;
 
 #define print_break_iter(bid, step) \
-  if (tid == 0 && false) printf("where to break: bid %d, step %d, line %d\n", bid, step, __LINE__);
+  if (tid == 0) printf("where to break: bid %d, step %d, line %d\n", bid, step, __LINE__);
 
 #define MSCCL_MAX_ITER 65536
 #define DEBUG_PRINT 0
