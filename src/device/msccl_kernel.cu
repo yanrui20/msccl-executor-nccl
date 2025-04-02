44fba369eae050f397b63420f125646656c8f9a2
yanrui <1657066071@qq.com>
2025-04-02 14:40:18 +0800

    打印数据块信息

--- src/device/msccl_kernel.cu
+++ src/device/msccl_kernel.cu
@@ -17,8 +17,6 @@
 
 __shared__ struct mscclShmemData mscclShmem;
 
-#define print_break_iter(bid, step) \
-  if (tid == 0 && false) printf("where to break: bid %d, step %d, line %d\n", bid, step, __LINE__);
 
 #define MSCCL_MAX_ITER 65536
 #define DEBUG_PRINT 0
@@ -182,6 +180,7 @@ __device__ __forceinline__ void mscclRunInterpreter(
   int sendPeer = mscclShmem.mscclTB.sendPeer;
 
   const ssize_t chunkSize = int(Proto::calcBytePerStep()/sizeof(T) * (Proto::Id == NCCL_PROTO_SIMPLE ? MSCCL_CHUNKSTEPS : 1));
+  if (tid == 0 && bid == 0) printf("mscclRunInterpreter: line %d, chunkSize %ld\n", __LINE__, chunkSize);
   int minChunkSize;
   if (Proto::Id == NCCL_PROTO_LL)
     minChunkSize = nthreads*(Proto::calcBytePerGrain()/sizeof(T));
@@ -201,13 +200,16 @@ __device__ __forceinline__ void mscclRunInterpreter(
 #endif
 
   const ssize_t sizePerMscclChunk = mscclShmem.work.count / mscclShmem.work.nChunksPerLoop;
+  if (tid == 0 && bid == 0) printf("mscclRunInterpreter: line %d, sizePerMscclChunk %ld\n", __LINE__, sizePerMscclChunk);
   uint32_t maxAllowedCount = mscclShmem.work.maxAllowedCount;
+  if (tid == 0 && bid == 0) printf("mscclRunInterpreter: line %d, maxAllowedCount %ld\n", __LINE__, maxAllowedCount);
 
   // msccl flags all start out with 0. this is used as a part of the flag to make sure different work items deal with different synchronization flags
   // this still needs more work. when we make a way around the queue, the flag might have been set to undesired values. will be fixed in subsequent versions.
   const int64_t workIndex = mscclShmem.work.workIndex;
   volatile struct mscclFlag* mscclFlags = mscclShmem.work.syncFlags;
   for (ssize_t gridOffset = 0, iter = 0; gridOffset < sizePerMscclChunk; gridOffset += chunkSize, iter++) {
+    if (tid == 0 && bid == 0) printf("mscclRunInterpreter: line %d, gridOffset %ld\n", __LINE__, gridOffset);
     ssize_t realChunkSize;
     if (Proto::Id == NCCL_PROTO_SIMPLE) {
       realChunkSize = min(chunkSize, sizePerMscclChunk-gridOffset);
@@ -223,23 +225,19 @@ __device__ __forceinline__ void mscclRunInterpreter(
     T *srcPointer, *dstPointer;
     int step = 0;
     for (int i = 0; i < mscclShmem.mscclTB.nSteps; i++){
-      print_break_iter(bid, i);
       struct mscclTransmission* t = &mscclShmem.mscclTB.transmissions[i];
+      if (tid == 0 && bid == 0) printf("mscclRunInterpreter: line %d, t->srcOffset %ld\n", __LINE__, t->srcOffset);
       // first wait if there is a dependence
       int16_t numDependencies = t->numDependencies;
-      print_break_iter(bid, i);
       if (numDependencies > 0){
         if (tid < numDependencies) {
           int16_t dependentPointer = t->dependencePointer;
           int16_t dependentBid = mscclShmem.mscclTB.dependentBid[dependentPointer+tid];
           int16_t dependentStep = mscclShmem.mscclTB.dependentStep[dependentPointer+tid];
           uint64_t goalFlag = COMPUTE_FLAG(workIndex, iter, dependentStep);
-          print_break_iter(bid, i);
           while (true){
             uint64_t curFlag = (mscclFlags + dependentBid)->flag;
-            print_break_iter(bid, i);
             if (curFlag >= goalFlag && GET_WORKINDEX_FROM_FLAG(curFlag) == workIndex) break;
-            print_break_iter(bid, i);
           }
         }
         step += numDependencies-1;
