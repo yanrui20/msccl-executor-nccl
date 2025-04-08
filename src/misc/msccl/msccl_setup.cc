b729af6855548f1a22fb24babdc80a3ed640e5d3
yanrui <1657066071@qq.com>
2025-04-08 17:41:33 +0800

    减少输出

--- src/misc/msccl/msccl_setup.cc
+++ src/misc/msccl/msccl_setup.cc
@@ -58,8 +58,6 @@ ncclResult_t mscclSetupCount(struct mscclAlgo* hostAlgo, ncclComm_t comm, size_t
     return ncclInternalError;
   }
   if (status.maxAllowedCount >= MSCCL_MAX_COUNT) {
-    INFO(NCCL_ALL, "MSCCL_DEBUG: maxAllowedCount(%u) >= 72\n", status.maxAllowedCount);
-    INFO(NCCL_ALL, "MSCCL_DEBUG: count %lu, type size %d, sizeMultiplier %d \n", count, ncclTypeSize(status.dataType), hostAlgo->sizeMultiplier);
     status.maxAllowedCount = MSCCL_MAX_COUNT - 1;
   }
   return ncclSuccess;
